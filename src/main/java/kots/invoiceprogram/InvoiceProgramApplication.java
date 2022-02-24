ec5782a58b54a84f24647546cefdcbb51ac0f0ef
SebaaK <sebaa.kot@gmail.com>
2022-02-24 17:29:02 +0100

    Init project

---
+++ src/main/java/kots/invoiceprogram/InvoiceProgramApplication.java
@@ -0,0 +1,13 @@
+package kots.invoiceprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kots/invoiceprogram/InvoiceProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package kots.invoiceprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
