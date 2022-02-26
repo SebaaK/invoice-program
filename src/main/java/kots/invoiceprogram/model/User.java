884dd936972b6d0cfd9ad20417d562dc9d6035e3
SebaaK <sebaa.kot@gmail.com>
2022-02-26 18:16:29 +0100

    Slimming project #todo

--- src/main/java/kots/invoiceprogram/model/User.java
+++ src/main/java/kots/invoiceprogram/model/User.java
@@ -6,26 +6,25 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
-@Getter
-@Setter
-@Table(name = "USERS")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne
-    @Column(name = "businessId")
-    private Business business;
-
-    @Enumerated(EnumType.STRING)
-    private GroupUser groupName;
-
-    private String username;
-    private String password;
-    private String email;
-    private Boolean enabled;
-
+//@Entity
+//@Getter
+//@Setter
+//@Table(name = "USERS")
+class User {
+////
+////    @Id
+////    @GeneratedValue(strategy = GenerationType.IDENTITY)
+////    private Long id;
+////
+////    @ManyToOne
+////    @Column(name = "businessId")
+////    private Business business;
+////
+////    @Enumerated(EnumType.STRING)
+////    private GroupUser groupName;
+////
+////    private String username;
+////    private String password;
+////    private String email;
+//    private Boolean enabled;
 }
