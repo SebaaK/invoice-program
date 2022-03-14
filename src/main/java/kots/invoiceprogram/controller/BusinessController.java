764893b6331199970e0469aaac2ca121db47d041
SebaaK <sebaa.kot@gmail.com>
2022-03-14 13:45:39 +0100

    Replace cron formule and add CrossOrigin for BusinessController

--- src/main/java/kots/invoiceprogram/controller/BusinessController.java
+++ src/main/java/kots/invoiceprogram/controller/BusinessController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/business")
+@CrossOrigin("*")
 class BusinessController {
 
     private final BusinessService service;
